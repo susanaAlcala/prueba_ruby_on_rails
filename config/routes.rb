d922012929711ab80cebd5219fe40a5db14f4ddf
Susana Alcala <susi.alcala@gmail.com>
2019-06-04 13:24:35 -0400

    Se realiza cambios a Complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
 get '/todos', to: 'todos#index'
 get 'todos/new'
 post '/todos', to: 'todos#create'
-get '/todo/list'
+#get '/todo/list'
 get '/todos/:id', to: 'todos#show', as: 'todo'
 get '/todos/:id/edit', to: 'todos#edit', as: 'edit_todo'
 patch '/todos/:id', to: 'todos#update'
 delete 'todos/:id', to: 'todos#destroy'
-get '/todos/:id/complete', to: 'todos#complete', as: 'complete_todo'
+post '/todos/:id/complete', to: 'todos#complete', as: 'complete_todo'
 root 'todos#index'
 
 end
