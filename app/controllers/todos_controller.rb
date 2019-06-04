528ce04806d2f7c0ac37874bd48070080e851cba
Susana Alcala <susi.alcala@gmail.com>
2019-06-04 11:53:59 -0400

    Se agrega Create y Show

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,32 @@
 class TodosController < ApplicationController
-def index
+    before_action :todo_find, only: [:show, :edit, :update, :destroy, :complete]
+
+    def index
     @todos = Todo.all
 end
 
 def new
     @todo = Todo.new
 end
+
+def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_path
+end 
+
+def show
+end
+
+
+private 
+def todo_params
+    params.require(:todo).permit(:description, :completed)
+end
+
+def todo_find
+    @todo = Todo.find(params[:id])
+end
+
 
 end
